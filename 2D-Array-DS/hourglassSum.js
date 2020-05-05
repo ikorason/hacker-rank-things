015cf0a4d422397573e66f268abd7a647afcf93c
Abulizi Yilifan <abuliziyilifan@Abulizis-MacBook-Pro.local>
2020-05-05 00:28:51 -0400

    update

--- 2D-Array-DS/hourglassSum.js
+++ 2D-Array-DS/hourglassSum.js
@@ -1,3 +1,29 @@
+/**
+ * @param arr {Array}
+ */
+function hourglassSum(arr) {
+    let result = -63
+
+    for (let row = 0; row <= arr.length - 3; row++) {
+        for (let col = 0; col <= arr.length - 3; col++) {
+            // sum for the top hourglass
+            let sum = arr[row][col] + arr[row][col + 1] + arr[row][col + 2]
+
+            // add the middle of hourglass
+            sum += arr[row + 1][col + 1]
+
+            // add the bottom of hourglass
+            sum += arr[row + 2][col] + arr[row + 2][col + 1] + arr[row + 2][col + 2]
+
+            if (result < sum) {
+                result = sum 
+            }
+        }
+    }
+
+    return result
+}
+
 // test
 const arr = [
     [1, 1, 1, 0, 0, 0],
@@ -7,30 +33,5 @@ const arr = [
     [0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0]
 ];
-
-/**
- * @param arr {Array}
- */
-function hourglassSum(arr) {
-    let hourglasses = []
-
-    for (let row = 0; row <= 3; row++) {
-        for (let col = 0; col <= 3; col++) {
-            let sum = 0
-            
-            sum += arr[row][col]
-            sum += arr[row][col + 1]
-            sum += arr[row][col + 2]
-            sum += arr[row + 1][col + 1]
-            sum += arr[row + 2][col]
-            sum += arr[row + 2][col + 1]
-            sum += arr[row + 2][col + 2]
-    
-            hourglasses.push(sum)
-        }
-    }
-
-    return hourglasses.length > 0 ? Math.max(...hourglasses) : 0
-}
 
 console.log(hourglassSum(arr))
